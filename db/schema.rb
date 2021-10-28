c3c0dbfd2ce72def32f78feeaa80e7e8590f9e09
kayakudance <kayaku2404@gmail.com>
2021-10-28 22:21:04 +0900

    product view　設定完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_28_003841) do
+ActiveRecord::Schema.define(version: 2021_10_28_062434) do
+
+  create_table "products", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.string "image_path"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |i|
+  Product.create(
+    name: "ダミー商品#{i + 1}",
+    description: "この商品はダミー商品#{i + 1}です。",
+    price:  100 + 100 * i,
+    image_path: "/images/dummy.jpeg"
+  )
+end

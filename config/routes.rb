cc4644a9205bf8b6cefb12a725f1c7d0bdd0ad73
kayakudance <kayaku2404@gmail.com>
2021-10-28 11:16:06 +0900

    devise view new form_for -> form_with　に修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users, cotroller: {
+  devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
     passwords: 'users/passwords',

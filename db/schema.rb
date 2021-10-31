74f29e12c91e9959792696ab005a4c9ab486f3aa
kayakudance <kayaku2404@gmail.com>
2021-10-31 21:08:52 +0900

    管理画面編集スタート devise初期設定途中 10/31作業終了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_31_045541) do
+ActiveRecord::Schema.define(version: 2021_10_31_104733) do
+
+  create_table "admins", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "cart_items", charset: "utf8", force: :cascade do |t|
     t.bigint "product_id", null: false
